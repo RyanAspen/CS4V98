e0f348451c19d7486df7d54e1aaa10079b36d310
RyanAspen <ryanaspen27@gmail.com>
2023-03-17 13:49:47 -0500

    various misc additions

---
+++ utils.py
@@ -0,0 +1,20 @@
+"""
+
+Utility methods used by other objects.
+
+"""
+
+def initialize_vision_ranges(environment):
+    pass
+
+def can_camera_see_camera(camera1, camera2):
+    pass
+
+def can_camera_see_object(camera, object):
+    pass
+
+def get_object_appearance(camera, object):
+    pass
+
+def send_handshake(sender, receiver):
+    pass
